ea2becb07b90851a1083921f6d4fea4da224c8d8
5c4lar <qinsiliang18@mails.ucas.ac.cn>
2024-01-16 21:45:41 +0800

    adjust training settings

--- sweep.py
+++ sweep.py
@@ -14,13 +14,13 @@ import wandb
 # Define sweep config
 sweep_configuration = {
     "program": "train.py",
-    "command": ["/usr/bin/env", "torchrun", "--standalone", "--nproc_per_node=1", "train.py", '--outdir=logs', '--cond=0', '--arch=ddpmpp', '--augment=0.0', '--window-size=0.0', '--precond=vp', '--seed=1024', '--duration=2000', '--num-blocks=2', '--fp16=True', '--lr=2e-4', '--batch=512', '--wandb_group=width', '${args}'],
+    "command": ["/usr/bin/env", "torchrun", "--standalone", "--nproc_per_node=1", "train.py", '--outdir=logs', '--cond=0', '--arch=ddpmpp', '--augment=0.0', '--window-size=0.0', '--precond=vp', '--seed=1024', '--duration=200', '--num-blocks=2', '--fp16=True', '--lr=2e-4', '--batch=512', '--wandb_group=width', '${args}'],
     "method": "grid",
     "name": "sweep",
     "metric": {"goal": "minimize", "name": "train_loss"},
     "parameters": {
-        "data": {"values": [1000, 2000, 4000, 8000, 16000, 32000]},
         "num-channels": {"values": [8, 16, 32, 64, 128, 256, 512]},
+        "data": {"values": [1000, 2000, 4000, 8000, 16000, 32000]},
     },
 }
 sweep_id = wandb.sweep(sweep_configuration, project="DiffusionUnlearning")
