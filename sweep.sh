b483033c0f4da254ad63fbfaaf65044fefed5971
5c4lar <qinsiliang18@mails.ucas.ac.cn>
2024-01-16 21:50:36 +0800

    use four procs

--- sweep.sh
+++ sweep.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-SWEEP_ID="ebumls3r"
+SWEEP_ID="Your Sweep ID"
 
 # Create sweeps for each of the 4 GPUs
-for i in {0..3}
-do
-    CUDA_VISIBLE_DEVICES=$i wandb agent $SWEEP_ID &
-done
+# for i in {0..3}
+# do
+#     CUDA_VISIBLE_DEVICES=$i wandb agent $SWEEP_ID &
+# done
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 wandb agent $SWEEP_ID &
